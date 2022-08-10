d936d9477ae02a13ec92f6b6946359a16a4c4981
hrenaud (NovaGaïa) <renaud.heluin@novagaia.com>
2022-08-10 16:25:20 +0200

    add conf

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,27 @@
 module.exports = {
   siteMetadata: {
     title: `Poc Gatsby GitHub Pages`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  pathPrefix: '/poc-gatsby-git-hub-pages',
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
     },
-    __key: "images"
-  }]
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
 };
